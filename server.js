76e9ce815fa28baaeab5a2aa33df5b9036b83100
xuboxun <huangyifan0119@163.com>
2019-07-10 15:12:03 +0800

    express server

--- server.js
+++ server.js
@@ -1,34 +1,33 @@
 const fs = require('fs');
-const Vue = require('vue');
+const path = require('path');
 const express = require('express');
-const { createRenderer } = require('vue-server-renderer');
+const { createBundleRenderer } = require('vue-server-renderer');
+
+const template =  fs.readFileSync('./src/index.template.html', 'utf-8');
+const serverBundle = require('./dist/vue-ssr-server-bundle.json');
+const clientManifest = require('./dist/vue-ssr-client-manifest.json');
+
+const renderer = createBundleRenderer(serverBundle, {
+    runInNewContext: false,
+    basedir: path.resolve(__dirname, './'),
+    template,
+    clientManifest
+});
 
 const server = express();
 
-const template =  fs.readFileSync('./src/index.template.html', 'utf-8');
-
-const renderer = createRenderer({
-    template
-});
+// # 使用此行后会被客户端接管，data-server-rendered="true"在接管后消失
+server.use('/dist', express.static(path.resolve(__dirname, './dist')));
 
 server.get('*', (req, res) => {
-    const app = new Vue({
-        data: {
-            url: req.url
-        },
-        template: `<div>
-                <h1>ssr content</h1>
-                <p>the url you requested is {{url}}</p>
-            </div>
-        `
-    });
+    const context = { url: req.url };
 
-    renderer.renderToString(app, (err, html) => {
+    renderer.renderToString(context, (err, html) => {
         if (err) {
-            res.status(500).end('Internal Server Error');
-            return;
+            res.redirect('/error');
+        } else {
+            res.end(html);
         }
-        res.end(html);
     })
 });
 
